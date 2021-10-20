c3678c4bf6fb600e1fc965338fd3c74946fb95d3
unknown <juslepi@gmail.com>
2021-10-20 05:16:35 +0300

    Better scalability

--- move_files.py
+++ move_files.py
@@ -5,25 +5,31 @@ folder_path = os.path.realpath("C:/Users/Jussi/Downloads")
 files = os.listdir(folder_path)
 
 # Files to be affected
-guitarPro = {
-    "destination" : os.path.realpath("C:/Users/Jussi/Documents/Tabs/moved-tabs"),
-    "extensions" : ["gpx","gp2","gp3","gp4","gp5","gp6","gp7"]
-}
-
-os.rename
+target_files = [
+     {  
+        "name" : "Guitar Pro",
+        "destination" : os.path.realpath("C:/Users/Jussi/Documents/Tabs/moved-tabs"),
+        "extensions" : ["gpx","gp2","gp3","gp4","gp5","gp6","gp7"]
+    },
+    {
+        "name": "Images",
+        "destination" : os.path.realpath("C:/Users/Jussi/Documents/Tabs/moved-images"),
+        "extensions" : ["jpeg", "jpg", "png"]
+    }
+]
 
 for file in files:
     filename, extension = os.path.splitext(file)
     current_file = os.path.join(folder_path, filename + extension)
-    destination = guitarPro["destination"]
+    
+    for target in target_files:
+        if extension.split(".")[-1].lower() in target["extensions"]:
+            try:
+                shutil.move(current_file, target["destination"])
+                print(filename, extension, "moved to", target["destination"])
+            except(shutil.Error):
+                print(filename, extension, "already exists in destination folder.")
 
-    if extension.split(".")[1] in guitarPro["extensions"]:
-        try:
-            shutil.move(current_file, destination)
-            print(f"{filename + extension} moved to {destination}")
-        except (shutil.Error):
-            print(filename, "already exists in destination folder.")
 
 
-
-input("enter")
+input("Enter to continue...")
